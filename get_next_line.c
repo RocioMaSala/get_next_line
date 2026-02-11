9b6211639a24b2a2f9b58058251b546a096dfb79
RocioMaSala <martinrocio16@gmail.com>
2026-02-11 18:55:21 +0100

    Versión 1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: romarti2 <romarti2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 16:43:54 by romarti2          #+#    #+#             */
-/*   Updated: 2026/02/11 16:50:03 by romarti2         ###   ########.fr       */
+/*   Updated: 2026/02/11 18:29:00 by romarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,26 @@ char *get_next_line(int fd)
 {
     
 
+    
+
+    
+}
+
+ssize_t read(int fildes, void *buf, size_t nbyte)
+{
 
 
 
     
+}
+
+
+
+int open (const char* path, int flags [, int mode ])
+{
+    
+
+
 
     
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,23 @@
 /*   By: romarti2 <romarti2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 16:48:59 by romarti2          #+#    #+#             */
-/*   Updated: 2026/02/11 16:49:04 by romarti2         ###   ########.fr       */
+/*   Updated: 2026/02/11 18:15:31 by romarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <fcntl.h> // Para read
+# include <unistd.h>
+
+int	ft_hexa(unsigned long num);
+int	ft_hexamay(unsigned long num);
+int	ft_printchar(int c);
+int	ft_printf(char const *s, ...);
+int	ft_printpunt(void *ptr);
+int	ft_printstring(char *s);
+int	ft_putnbr(int n);
+int	ft_uint(unsigned int n);
+
+#endif
